8148db22986f2df239fd26481a797535130fea52
pcSharer <53900900+pcSharer@users.noreply.github.com>
2020-04-20 22:06:36 +1000

    first commit

---
+++ flutter_service_worker.js
@@ -0,0 +1,39 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "assets/AssetManifest.json": "3a1a25fc13ec0260a7852156dbee4d3d",
+"assets/FontManifest.json": "0a8d34b221a73ebaa0b1afbffbba0193",
+"assets/fonts/Corben/Corben-Bold.ttf": "8f9921f9c52d3c25fd354d6e01f7b024",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/LICENSE": "e791e3aa37dd1b6df210fe3efb183398",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"index.html": "32a753cd40727968c29938c1c32a2fe2",
+"/": "32a753cd40727968c29938c1c32a2fe2",
+"main.dart.js": "3b8253521a663b8d6e4dff83c31e376e",
+"manifest.json": "5bfaa8407b6bbc7d3ec933b8243f9786"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
